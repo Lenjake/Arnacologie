c953c709e57c799ac50a6e22ed2883723dd9fd83
Lenjake <rmanu26e@gmail.com>
2017-11-07 01:12:30 +0100

    Add files via upload

---
+++ js/controls.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('header a').click(function(e){
+		$.scrollTo(this.hash||0,850);
+		e.preventDefault();
+		});
+		/****************END************/
+		/************Sous - Liste*******/
+
+	});
